72aa5067d451a5c00715ee091aa887648fae45c6
Aditya Jain <adityajain@Adityas-MacBook-Air.local>
2025-12-31 13:01:09 +0530

    chore: convert apps and packages to independent repositories

--- apps/admin/android/app/src/main/kotlin/com/factoryflow/admin_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.factoryflow.admin_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- apps/worker/android/app/src/main/kotlin/com/factoryflow/worker_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.factoryflow.worker_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
